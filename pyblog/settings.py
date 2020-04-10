14087143b70ef495bb67c901ff79c56f69978ccc
TristanLa <latrasse.tristan@gmail.com>
2020-04-10 16:23:21 +0200

    for deployment

--- pyblog/settings.py
+++ pyblog/settings.py
@@ -122,7 +122,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/static-res/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'static-res'),
 ]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+from pyblog.local_settings import *
