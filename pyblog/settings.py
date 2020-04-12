dfefb7646934ffb2d39072f77967d9ba3a1e516b
TristanLa <latrasse.tristan@gmail.com>
2020-04-12 13:45:22 +0200

    Make static work

--- pyblog/settings.py
+++ pyblog/settings.py
@@ -129,17 +129,20 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
 if os.environ.get('ENV') == "PRODUCTION":
     PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
-    STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+    STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
     STATICFILES_DIRS = (
         os.path.join(PROJECT_ROOT, 'static'),
                         )
-
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
     db_from_env = dj_database_url.config(conn_max_age=500)
     DATABASES['default'].update(db_from_env)
+
 
 django_heroku.settings(locals())
 
