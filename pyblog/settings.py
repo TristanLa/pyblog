66cf704eec3f21b5cd4b5a6ad66cf82f5c103932
TristanLa <latrasse.tristan@gmail.com>
2020-04-12 13:47:06 +0200

    fix static 2

--- pyblog/settings.py
+++ pyblog/settings.py
@@ -135,7 +135,7 @@ STATICFILES_DIRS = (
 
 if os.environ.get('ENV') == "PRODUCTION":
     PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
-    STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
+    STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
     STATICFILES_DIRS = (
         os.path.join(PROJECT_ROOT, 'static'),
                         )
